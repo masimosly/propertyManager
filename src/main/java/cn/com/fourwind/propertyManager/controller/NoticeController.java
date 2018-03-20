fef974c24e20618b5c6c385be573d39a36cabb08
masimos <baggioliuyang@163.com>
2018-03-20 11:14:19 +0800

    跨域

--- src/main/java/cn/com/fourwind/propertyManager/controller/NoticeController.java
+++ src/main/java/cn/com/fourwind/propertyManager/controller/NoticeController.java
@@ -1,11 +1,13 @@
 package cn.com.fourwind.propertyManager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.com.fourwind.propertyManager.service.NoticeService;
 
 @RestController
+@CrossOrigin(origins="*",maxAge=3600)
 public class NoticeController {
 	
 	@Autowired
